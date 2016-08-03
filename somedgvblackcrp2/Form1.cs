91d9464bd5874b7c10d2be1ff2c3f92cdba46e2c
david miscem <davidmiscem@gmail.com>
2016-08-03 18:14:48 +0100

    first commit

---
+++ somedgvblackcrp2/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace somedgvblackcrp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.Columns.Add("Column1", "Column1");
+            dataGridView1.Rows.Add(2);
+            dataGridView1.AllowUserToAddRows = false;
+            /*
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            */
+
+            //MessageBox.Show("ASDF="+SystemInformation.BorderSize.Width.ToString());
+            // jkjkhj dataGridView1.Columns[0].Width = 100;
+
+        }
+
+        private void dataGridView1_EditingControlShowing(object sender, DataGridViewEditingControlShowingEventArgs e)
+        {
+            /*
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            */
+            if (e.Control is TextBox)
+            {
+                var tbox = (e.Control as TextBox);
+
+                tbox.TextChanged -= A_Cell_TextChanged;
+                tbox.TextChanged += A_Cell_TextChanged;
+            }
+
+
+        }
+
+        private void A_Cell_TextChanged(object sender, EventArgs e)
+        {
+            /*
+            dataGridView1.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            */
+
+
+        }
+    }
+}
